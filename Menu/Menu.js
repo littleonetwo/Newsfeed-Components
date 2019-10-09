fc98c883130e8cca538163864ed4cb9580bef6a7
littleonetwo <littleonetwo@gmail.com>
2019-10-08 22:29:20 -0500

    finished assignment no stretch

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -9,7 +9,7 @@ let menuItems = [
   'Log Out'
 ];
 
-/* 
+/*
 
   Step 1: Write a function that will create a menu component as seen below:
 
@@ -21,7 +21,7 @@ let menuItems = [
 
   The function takes an array as its only argument.
 
-  Step 2: Inside this function, iterate over the array creating a list item <li> element for each item in the array. 
+  Step 2: Inside this function, iterate over the array creating a list item <li> element for each item in the array.
   Add those items to the <ul>
 
   Step 3: Using a DOM selector, select the menu button (the element with a class of 'menu-button') currently on the DOM.
@@ -31,5 +31,52 @@ let menuItems = [
   Step 5: return the menu component.
 
   Step 6: add the menu component to the DOM.
-  
+
 */
+
+function createMenu(info){
+
+
+  let newMenu1 = document.createElement('div');
+  let newMenu2 = document.createElement('ul');
+
+  newMenu1.classList.add('menu');
+
+  let final = newMenu1;
+  let menu = newMenu2;
+
+  let count = 0;
+
+  while(count < info.length){
+    let newMenu3 = document.createElement('li');
+    newMenu3.textContent = info[count];
+    menu.appendChild(newMenu3);
+    count ++;
+  };
+
+
+
+  final.appendChild(menu);
+
+
+
+  return final;
+
+};
+
+
+let newMenu = createMenu(menuItems);
+
+
+menuSelect = document.querySelector('.menu-button');
+header = document.querySelector('.header');
+header.appendChild(newMenu);
+
+
+
+menuSelect2 = document.querySelector('.menu');
+
+menuSelect.addEventListener('click', (e)=>{
+  menuSelect2.classList.toggle('menu--open');
+
+});
